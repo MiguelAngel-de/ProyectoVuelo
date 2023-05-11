f7feb7a0f2845984b11300c8c0703632b95444ec
Miguel <mbermudez@itpfp.com>
2023-05-11 09:21:48 +0200

    proyecto terminado, para provar

--- src/paquete/App.java
+++ src/paquete/App.java
@@ -1,9 +1,30 @@
 package paquete;
-
+import java.util.Scanner;
 public class App {
+
     public static void main(String[] args) throws Exception {
+        Scanner leer = new Scanner(System.in);
+        int op;
         GestionVuelo vuelo=new GestionVuelo();
-        vuelo.agregarVuelo();
-        
+       
+        do{
+           System.out.println("1 para agregar vuelo. \n2 para modificar vuelo. \n3 para eliminar vuelo \n0 para salir de la aplicacion.");
+           op=leer.nextInt();
+            switch(op){
+                case 1:
+                vuelo.agregarVuelo();
+                break;
+                case 2:
+                vuelo.modificarVuelo();
+                break;
+                case 3:
+                vuelo.eliminarVuelo();
+                break;
+                default:
+                System.out.println("numero incorrecto, introduzca el numero correspondiente por favor");
+
+            }
+
+        }while(op!=0);
     }
 }
